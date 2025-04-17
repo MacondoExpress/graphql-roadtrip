ef5062cd6901df7a49a717c8bf622a7f62bd1164
MacondoExpress <simone.gammicchia@gmail.com>
2025-04-17 14:26:05 +0100

    Fix merge conflicts
    Co-authored-by: MacondoExpress <simone.gammicchia@gmail.com>
    Co-authored-by: angrykoala <angrykoala@outlook.es>

--- index.ts
+++ index.ts
@@ -3,35 +3,11 @@ import { ASTVisitor, DocumentNode, parse, print, visit } from "graphql";
 import { fulltextRequiredParameter } from "./rules/fulltext-required-parameters";
 import { relationshipQueryDirection } from "./rules/relationship-query-direction";
 import { removePrivateDirective } from "./rules/remove-private-directive";
-<<<<<<< HEAD
 import { makeListFieldNonNullable } from "./rules/make-list-field-non-nullable";
-||||||| 07b5d21
-=======
 import { removeUniqueDirective } from "./rules/remove-unique-directive";
 import { requireNodeDirective } from "./rules/required-node-directive";
->>>>>>> 79a3d8be7df1be3834edafddcc99a7c75dc04e93
 
 function main() {
-<<<<<<< HEAD
-  const typedefs = fs.readFileSync("typedef.graphql", { encoding: "utf-8" });
-  console.log(typedefs);
-  const ast = parse(typedefs);
-  const rules = [
-    removeUniqueDirective,
-    requireNodeDirective,
-    removePrivateDirective,
-    makeListFieldNonNullable,
-  ];
-||||||| 07b5d21
-  const typedefs = fs.readFileSync("typedef.graphql", { encoding: "utf-8" });
-  console.log(typedefs);
-  const ast = parse(typedefs);
-  const rules = [
-    removeUniqueDirective,
-    requireNodeDirective,
-    removePrivateDirective,
-  ];
-=======
     const typedefs = fs.readFileSync("typedef.graphql", { encoding: "utf-8" });
     console.log(typedefs);
     const ast = parse(typedefs);
@@ -41,8 +17,8 @@ function main() {
         removePrivateDirective,
         relationshipQueryDirection,
         fulltextRequiredParameter,
+        makeListFieldNonNullable,
     ];
->>>>>>> 79a3d8be7df1be3834edafddcc99a7c75dc04e93
 
     const result = migrate(ast, rules);
     console.log("-----");
