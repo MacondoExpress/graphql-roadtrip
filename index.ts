e01b94985272a1a40edfacc819573ebd90930424
angrykoala <angrykoala@outlook.es>
2025-04-17 13:23:54 +0100

    queryDirection Rule
    Co-authored-by: MacondoExpress <simone.gammicchia@gmail.com>
    Co-authored-by: angrykoala <angrykoala@outlook.es>

--- index.ts
+++ index.ts
@@ -1,37 +1,27 @@
 import * as fs from "fs";
-import { requireNodeDirective } from "./rules/required-node-directive";
-import { removeUniqueDirective } from "./rules/remove-unique-directive";
-import {
-  ASTVisitFn,
-  ASTVisitor,
-  DocumentNode,
-  parse,
-  print,
-  visit,
-} from "graphql";
+import { ASTVisitor, DocumentNode, parse, print, visit } from "graphql";
+import { relationshipQueryDirection } from "./rules/relationship-query-direction";
 import { removePrivateDirective } from "./rules/remove-private-directive";
+import { removeUniqueDirective } from "./rules/remove-unique-directive";
+import { requireNodeDirective } from "./rules/required-node-directive";
 
 function main() {
-  const typedefs = fs.readFileSync("typedef.graphql", { encoding: "utf-8" });
-  console.log(typedefs);
-  const ast = parse(typedefs);
-  const rules = [
-    removeUniqueDirective,
-    requireNodeDirective,
-    removePrivateDirective,
-  ];
+    const typedefs = fs.readFileSync("typedef.graphql", { encoding: "utf-8" });
+    console.log(typedefs);
+    const ast = parse(typedefs);
+    const rules = [removeUniqueDirective, requireNodeDirective, removePrivateDirective, relationshipQueryDirection];
 
-  const result = migrate(ast, rules);
-  console.log("-----");
-  const updatedTypedefs = print(result);
-  console.log(updatedTypedefs);
+    const result = migrate(ast, rules);
+    console.log("-----");
+    const updatedTypedefs = print(result);
+    console.log(updatedTypedefs);
 }
 
 function migrate(ast: DocumentNode, rules: Array<ASTVisitor>): DocumentNode {
-  return rules.reduce((acc, rule) => {
-    const intermediate = visit(acc, rule);
-    return intermediate;
-  }, ast);
+    return rules.reduce((acc, rule) => {
+        const intermediate = visit(acc, rule);
+        return intermediate;
+    }, ast);
 }
 
 main();
